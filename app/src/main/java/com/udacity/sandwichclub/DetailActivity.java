4fb986df116570f24c27234cbd3cacceb1e644cb
joycelin12 <joycelin12@hotmail.com>
2018-04-29 20:06:37 -0400

    JSON data is parsed correctly to a Sandwich object in JsonUtils JSON is parsed without using 3rd party libraries DetailActivity shows all Sandwich details correctly Detail layout includes a ScrollView so all the details are visible in small screen devices Sandwich details are shown in a sensible layout. For example, ingredients appear next to the ingredients label

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.json.JSONException;
+
+import java.util.Arrays;
+
+import static com.udacity.sandwichclub.utils.JsonUtils.parseSandwichJson;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -36,14 +44,19 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich = null;
+        try {
+            sandwich = parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +69,23 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        //get all the textview by their id
+        TextView alsoKnownAsTextView = findViewById(R.id.also_known_tv);
+        TextView originTextView = findViewById(R.id.origin_tv);
+        TextView descriptionTextView = findViewById(R.id.description_tv);
+        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
+
+        //set the textview with the text
+        //to convert arrayliststring to charseqence https://stackoverflow.com/questions/3032342/arrayliststring-to-charsequence
+        //remove the brackets using the url: https://stackoverflow.com/questions/7536154/remove-brackets-from-a-list-set-to-a-textview
+        alsoKnownAsTextView.setText(Arrays.toString(sandwich.getAlsoKnownAs().toArray(new CharSequence[sandwich.getAlsoKnownAs().size()])).replace("[", "").replace("]", ""));
+        originTextView.setText(sandwich.getPlaceOfOrigin());
+        descriptionTextView.setText(sandwich.getDescription());
+        ingredientsTextView.setText(Arrays.toString(sandwich.getIngredients().toArray(new CharSequence[sandwich.getIngredients().size()])).replace("[", "").replace("]", ""));
+
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,61 @@
 package com.udacity.sandwichclub.utils;
 
+import android.support.annotation.NonNull;
+
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+
+        //create a new variable to store sandwich details
+        Sandwich details = new Sandwich();
+
+        //referencing from the url:https://www.androidhive.info/2012/01/android-json-parsing-tutorial/
+        //on parsing json =  {"name":{"mainName":"Ham and cheese sandwich","alsoKnownAs":[]},"placeOfOrigin":"","description":"A ham and cheese sandwich is a common type of sandwich. It is made by putting cheese and sliced ham between two slices of bread. The bread is sometimes buttered and/or toasted. Vegetables like lettuce, tomato, onion or pickle slices can also be included. Various kinds of mustard and mayonnaise are also common.","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG","ingredients":["Sliced bread","Cheese","Ham"]}
+        JSONObject jsonObj = new JSONObject(json);
+
+        // Getting JSON Object node
+        JSONObject nameObj = jsonObj.getJSONObject("name");
+        String mainName = nameObj.getString("mainName");
+        // When there is square bracket have to use JSONArray
+        JSONArray alsoKnownAsObj = nameObj.getJSONArray("alsoKnownAs");
+        List<String> alsoKnownAs = new ArrayList<String>();
+        for(int i =0; i<alsoKnownAsObj.length(); i++) {
+            alsoKnownAs.add(alsoKnownAsObj.getString(i));
+
+        }
+
+        String placeOfOrigin = jsonObj.getString("placeOfOrigin");
+        String description = jsonObj.getString("description");
+        String image = jsonObj.getString("image");
+        JSONArray ingredientsObj = jsonObj.getJSONArray("ingredients");
+        List<String> ingredients= new ArrayList<String>();
+        for(int j =0; j<ingredientsObj.length(); j++) {
+            ingredients.add(ingredientsObj.getString(j));
+
+        }
+
+        // Add the details to sandwich
+        details.setMainName(mainName);
+        details.setAlsoKnownAs(alsoKnownAs);
+        details.setPlaceOfOrigin(placeOfOrigin);
+        details.setDescription(description);
+        details.setImage(image);
+        details.setIngredients(ingredients);
+
+        System.out.println(placeOfOrigin+"testing");
+        return details;
     }
 }
